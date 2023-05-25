4917c598d64e7060b218a9c89f3335df1f31f8a5
Igor Martins <igorjose07@hotmail.com>
2023-05-25 13:41:06 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,59 @@
+public class App {
+//  declarar e atribuir valores em variaveis 
+//  Em Java, existem oito tipos de dados primitivos. Eles são:
+//  byte: Representa números inteiros de 8 bits. O intervalo válido é de -128 a 127.
+//  short: Representa números inteiros de 16 bits. O intervalo válido é de -32.768 a 32.767.
+//  int: Representa números inteiros de 32 bits. O intervalo válido é de -2.147.483.648 a 2.147.483.647.
+//  long: Representa números inteiros de 64 bits. O intervalo válido é de -9.223.372.036.854.775.808 a 9.223. 372.036.854.775.807.
+// float: Representa números de ponto flutuante de precisão simples de 32 bits.
+// double: Representa números de ponto flutuante de precisão dupla de 64 bits.
+// boolean: Representa um valor booleano, que pode ser verdadeiro (true) ou falso (false).
+// char: Representa um único caractere Unicode de 16 bits.
+// Esses tipos de dados primitivos são usados para declarar variáveis e armazenar valores básicos na linguagem Java.
+//  
+//  como criar variaveis para armazenar texto
+// numero inteiros e decimais 
+// operadores aritimedicos 
+    public static void main(String[] args) {
+
+        // String nome;
+        
+        // nome = "let`s code";
+        // nome = "Igor";
+        // nome = "Brasil";
+        // System.out.println("ola, " + nome); 
+
+        // int a; 
+        // int b = 2;
+        //  a = 3;
+        //  int soma = a + b;
+        //  int subtracao = a - b;
+        //  int multiplicacao = a * b;
+        //  int divisao = a / b;
+
+        //  System.out.println(soma);
+        //  System.out.println(subtracao);
+        //  System.out.println(multiplicacao);
+        //  System.out.println(divisao);
+
+
+
+          int a; 
+        int b = 2;
+         a = 3;
+         int soma = a + b;
+         int subtracao = a - b;
+         int multiplicacao = a * b;
+
+            // float le numero decimais
+         float divisao = (float) a / b;
+
+         System.out.println(soma);
+         System.out.println(subtracao);
+         System.out.println(multiplicacao);
+         System.out.println(divisao);
+         
+         
+
+    }
+}
