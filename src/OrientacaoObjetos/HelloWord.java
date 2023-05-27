6b908437a11f278ae03df8c5e0422658f0fe3c26
Igor Martins <igorjose07@hotmail.com>
2023-05-27 12:24:22 -0300

    ObCachorro

---
+++ src/OrientacaoObjetos/HelloWord.java
@@ -0,0 +1,20 @@
+package OrientacaoObjetos;
+
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+
+        Cachorro cachorro1 = cachorro1 = new Cachorro();
+
+        cachorro1.nome = "Puppy";
+        cachorro1.cor = "Marro";
+        cachorro1.altura = 25;
+        cachorro1.peso = 5.5;
+        cachorro1.tamanhoDoRabo = 5;
+
+        System.out.println(cachorro1);
+        
+    }
+    
+}
