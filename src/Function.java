f9edfb55b5fecd10eade2d4dae6f1f803a718341
Igor Martins <igorjose07@hotmail.com>
2023-05-27 09:23:19 -0300

    Vetores

---
+++ src/Function.java
@@ -0,0 +1,24 @@
+
+
+// metodos e function sao diferetentes 
+public class Function {
+    public static void main(String[] args) { 
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
+        
+    }
+    public static int soma( int a, int b) {
+        return a + b;
+     }
+        
+    //     //     // nome de escopo o mesmo nome que é setado. tem que voltar na funcao. 
+    //  public static void saudacao() { 
+    //              String nomeOriginal = "Igor Martins";
+    //              saudacao(nomeOriginal);   
+    // }
+        
+    //          // nome de escopo o mesmo nome que é setado. tem que voltar na funcao. 
+    // public static void saudacao(String nomeParametro) {
+    //          System.out.println("Hello, " + nomeParametro);
+    // }
+}
